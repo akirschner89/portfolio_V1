1f2dc03a99cd695d4e03e8fed4ef5c92b54f9516
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-07-30 11:20:02 -0500

    fixing css

--- js/front.js
+++ js/front.js
@@ -91,7 +91,7 @@ function map() {
 
         function initMap() {
 
-            var location = new google.maps.LatLng(50.0875726, 14.4189987);
+            var location = new google.maps.LatLng(30.267153, -97.7430608);
 
             var mapCanvas = document.getElementById('map');
             var mapOptions = {
@@ -102,18 +102,19 @@ function map() {
             }
             var map = new google.maps.Map(mapCanvas, mapOptions);
 
-            var markerImage = 'img/marker.png';
+            var markerImage = 'favicon.png';
 
             var marker = new google.maps.Marker({
                 position: location,
                 map: map,
-                icon: markerImage
+                icon: markerImage,
+                draggable: true
             });
 
             var contentString = '<div class="info-window">' +
-                    '<h3>Info Window Content</h3>' +
+                    '<h3>Take me somewhere</h3>' +
                     '<div class="info-content">' +
-                    '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                    '<p>Drag my bitmoji anywhere around the world! I\'m originally from New York, NY - feel free to start there</p>' +
                     '</div>' +
                     '</div>';
 
