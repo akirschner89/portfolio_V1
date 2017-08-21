617f341fba2ad013274c230fcd18845064d121cb
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-08-20 21:45:05 -0500

    email connect

--- js/front.js
+++ js/front.js
@@ -390,3 +390,63 @@ $(window).resize(function () {
     }
 
 });
+
+// //AJAX call to PHP - send email
+// module.exports = function app() {
+
+// function isValidEmail(emailAddress) {
+//     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+//     return pattern.test(emailAddress);
+
+// }
+
+// $("#contactForm").on('submit', function (e) {
+//     e.preventDefault();
+//     var data = {
+//         firstname: $("#name").val().trim(),
+//         lastname: $("#surnamename").val().trim(),
+//         email: $("#email").val().trim(),
+//         // subject: $("#subject").val().trim(),
+//         message: $("#message").val().trim()
+//     };
+
+//     if (isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) && (data['subject'].length > 1)) {
+//         $.ajax({
+//             type: "POST",
+//             url: "contact.php",
+//             data: data,
+//             success: function () {
+//                 $('#contactForm .input-success').delay(500).fadeIn(1000);
+//                 $('#contactForm .input-error').fadeOut(500);
+//             }
+//         });
+//     } else {
+//         $('#contactForm .input-error').delay(500).fadeIn(1000);
+//         $('#contactForm .input-success').fadeOut(500);
+//     }
+
+//     return false;
+// });
+
+// }
+    // $(".email-submit").on('click', function() {
+    //     var data = {
+    //     firstname: $("#name").val().trim(),
+    //     lastname: $("#surname").val().trim(),
+    //     email: $("#email").val().trim(),
+    //     message: $("#message").val().trim()
+    //     };
+
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "../contact.php",
+    //         data: data,
+    //         success: function(){
+    //         $('.success').fadeIn(1000);
+    //         }
+    //     });
+
+    //     return true;
+    // });
+
--- server.js
+++ server.js
@@ -3,4 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+// require('./js/emailScript.js')(app);
+
+
 app.listen(process.env.PORT || 8080);
